dc38c4bc1d55ba40ed3b06129e0c0e7707ceb25b
Bishal Aryal <077BCT021.bishal@gmail.com>
2024-03-25 20:51:03 +0545

    Updated code!

--- unetApp/views.py
+++ unetApp/views.py
@@ -1,29 +1,3 @@
-# from django.shortcuts import render
-# from .serializers import ImageSerializer
-# from .models import Images
-# from rest_framework import viewsets, status
-# from django.http import HttpResponse
-# from rest_framework.response import Response
-
-# # Create your views here.
-
-
-# class ImageViewSet(viewsets.ModelViewSet):
-#     queryset = Images.objects.all()
-#     serializer_class = ImageSerializer
-
-#     # def post(self, request, *args, **kwargs):
-#     #     unMaskedImage = request.data['unMaskedImage']
-#     #     Images.objects.create(unMaskedImage =unMaskedImage)
-#     #     return HttpResponse({'message':'Image Uploaded.'}, status = 200)
-
-#     def create(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         self.perform_create(serializer)
-#         headers = self.get_success_headers(serializer.data)
-#         return Response({'message': 'Image uploaded successfully', 'unMaskedImage': serializer.data['unMaskedImage']}, status=status.HTTP_201_CREATED, headers=headers)
-
 import tensorflow as tf
 from tensorflow import keras
 from keras import backend as K
@@ -60,6 +34,8 @@ class ImageViewSet(viewsets.ModelViewSet):
         resized_image = pil_image.resize((256, 256))
         # Convert the PIL image to a numpy array
         image = img_to_array(resized_image) / 255.0
+        
+        image = image[:, :, :3]
         image = np.expand_dims(image, axis=0)  # Add batch dimension
         return image
 
@@ -85,6 +61,33 @@ class ImageViewSet(viewsets.ModelViewSet):
                     # For example, set the pixel to black or any other color
                     image[j, i] = (0, 0, 0)  # Black color
         return image
+    
+    def calculate_area_percentage(self, mask_image):
+        class_color_map = {
+            "Building": [60, 16, 152],
+            "Land": [132, 41, 246],
+            "Road": [110, 193, 228],
+            "Vegetation": [254, 221, 58],
+            "Water": [226, 169, 41],
+            "Unlabeled": [155, 155, 155]
+        }
+        class_areas = {}
+
+        if mask_image.size == 0:
+            # Handle empty mask_image
+            return class_areas
+
+        mask_array = np.array(mask_image)
+        if len(mask_array.shape) != 3:
+            # Ensure mask_image has expected dimensions
+            return class_areas
+
+        total_pixels = mask_array.shape[0] * mask_array.shape[1]
+        for class_name, color in class_color_map.items():
+            class_pixels = np.sum(np.all(mask_array == color, axis=2))
+            class_area_percentage = (class_pixels / total_pixels) * 100
+            class_areas[class_name] = class_area_percentage
+        return class_areas
 
     def create(self, request, *args, **kwargs):
 
@@ -92,7 +95,16 @@ class ImageViewSet(viewsets.ModelViewSet):
 
         # Preprocess the image
         preprocessed_image = self.preprocess_image(uploaded_image)
+        
+        # Define RGB values for each class
+        class_building = np.array([60, 16, 152])  # RGB values for building
+        class_land = np.array([132, 41, 246])      # RGB values for land
+        class_road = np.array([110, 193, 228])     # RGB values for road
+        class_vegetation = np.array([254, 221, 58]) # RGB values for vegetation
+        class_water = np.array([226, 169, 41])      # RGB values for water
+        class_unlabeled = np.array([155, 155, 155]) # RGB values for unlabeled
 
+    
         def jaccard_coef(y_true, y_pred):
             y_true_flatten = K.flatten(y_true)
             y_pred_flatten = K.flatten(y_pred)
@@ -128,7 +140,7 @@ class ImageViewSet(viewsets.ModelViewSet):
             'dice_loss_plus_1focal_loss': dice_loss_plus_1focal_loss
         }
 
-        model_file_path = os.path.join(settings.BASE_DIR, 'segment_models', 'model50epoch.h5')
+        model_file_path = os.path.join(settings.BASE_DIR, 'segment_models', 'next25epochcrossvalidation.h5')
         
         metrics = ["accuracy", jaccard_coef]
         
@@ -151,6 +163,8 @@ class ImageViewSet(viewsets.ModelViewSet):
 
         # Postprocess the predicted mask
         predicted_mask = self.mask2img(predicted_mask)
+        # Calculate area percentage of each class in the predicted mask
+        class_areas = self.calculate_area_percentage(predicted_mask)
         # Save the predicted mask image to the masked directory
         # Assuming predicted_mask is a numpy array representing the image
         masked_image_path = os.path.join(
@@ -160,42 +174,18 @@ class ImageViewSet(viewsets.ModelViewSet):
 
         predicted_mask = Image.fromarray(predicted_mask)
 
-        # # Convert the PIL Image object to a byte array
-        # with io.BytesIO() as output:
-        #     predicted_mask.save(output, format='PNG')
-        #     predicted_mask_data = output.getvalue()
-
-        # # Save the predicted mask data to the database
-        # predicted_mask_file = ContentFile(predicted_mask_data)
-        
         # Save the PIL Image object to the specified path
         predicted_mask.save(masked_image_path)
 
         # Save the predicted mask file path to the database
         instance = Images.objects.create(unMaskedImage=uploaded_image, predictedMask=masked_image_path)
-        # Images.objects.create(unMaskedImage=uploaded_image, predictedMask=predicted_mask_file)
-
-        # # Serialize the image and mask to send back to the frontend
-        # serializer = ImageSerializer(
-        #     {'unMaskedImage': uploaded_image, 'predictedMask': predicted_mask})
-        # return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-        
-        # # Serialize the image and mask to send back to the frontend
-        # serializer = ImageSerializer(
-        #     {'unMaskedImageUrl': request.build_absolute_uri(unmasked_image_path),
-        #     'predictedMaskUrl': request.build_absolute_uri(masked_image_path)})
-        # return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        # serializer = self.get_serializer(data=request.data, context={'request': request})
-        # serializer.is_valid(raise_exception=True)
-        # self.perform_create(serializer)
-        # headers = self.get_success_headers(serializer.data)
-        # return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
         
         # Serialize the instance and include the predicted mask URL in the response
         serializer = self.get_serializer(instance)
         response_data = serializer.data
-        response_data['predictedMaskUrl'] = request.build_absolute_uri(instance.predictedMask.url)
+        response_data = {
+            'predictedMaskUrl': request.build_absolute_uri(instance.predictedMask.url),
+            'classAreas': class_areas
+        }
 
         return Response(response_data, status=status.HTTP_201_CREATED)
